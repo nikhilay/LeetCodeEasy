55e0b9184f96e50cbcef9481f89aba413e519ed5
nikhilay <nikhilayyagari15@gmail.com>
2016-10-17 14:54:16 -0400

    Leet Code easy contains duplicates solution

---
+++ src/ContainsDuplicates.java
@@ -0,0 +1,28 @@
+/**
+ * Created by Nikhil on 10/17/16.
+ */
+
+import java.util.HashSet;
+
+/**
+ * Given an array of integers, find if the array contains any duplicates. Your function should return true if any
+ * value appears at least twice in the array, and it should return false if every element is distinct.
+ */
+public class ContainsDuplicates {
+
+    public void checkIfitContainDuplicates(int[] nums) {
+        HashSet<Integer> hset = new HashSet<>();
+
+        for (int i : nums) {
+            if (hset.contains(i)) {
+                System.out.println("Contains duplicates");
+                return;
+            } else {
+                hset.add(i);
+            }
+        }
+
+        System.out.println("Does not contain duplicates");
+    }
+
+}
