38d9181a7e2d504b2b2dadbd7ac776ad4a472e37
nikhilay <nikhilayyagari15@gmail.com>
2016-12-25 20:25:37 -0500

    Swap nodes in pairs solution

--- src/SwapNodesInPairs.java
+++ src/SwapNodesInPairs.java
@@ -9,5 +9,26 @@
  * Your algorithm should use only constant space. You may not modify the values in the list, only nodes itself can be changed.
  */
 
+/**
+ * Inspired from
+ * https://discuss.leetcode.com/topic/4351/my-accepted-java-code-used-recursion
+ */
 public class SwapNodesInPairs {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode swapPairs(ListNode head) {
+        if(head==null || head.next==null) return head;
+        ListNode node = head.next;
+        head.next = swapPairs(head.next.next);
+        node.next = head;
+        return node;
+
+    }
 }
