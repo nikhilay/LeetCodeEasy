f11064cc9fbe416615dc69dfae1382b36d744d10
nikhilay <nikhilayyagari15@gmail.com>
2016-12-28 14:49:41 -0500

    Added faster and better method solution

--- src/PalindromePermutation.java
+++ src/PalindromePermutation.java
@@ -1,34 +1,26 @@
-import com.sun.org.apache.bcel.internal.generic.RETURN;
-
-import java.util.HashMap;
 
 /**
  * Created by Nikhil on 10/17/16.
  */
-public class PalindromePermutation {
-    public static void main(String[] args) {
-       boolean result = new PalindromePermutation().checkIfPalindromeisPossible("carerac");
-        System.out.println("Is the given String palindrome? "+result);
-    }
 
-    public boolean checkIfPalindromeisPossible(String s){
-        HashMap<Character, Integer> hmap = new HashMap<>();
+/**
+ * Given a string, determine if a permutation of the string could form a palindrome.
+ * For example,
+ * "code" -> False, "aab" -> True, "carerac" -> True.
+ */
+public class PalindromePermutation {
+
+    public boolean checkIfPalindromeisPossible(String s) {
+        int[] helper = new int[256];
         for (int i = 0; i < s.length(); i++) {
             Character c = s.charAt(i);
-            if (hmap.containsKey(c)) {
-                int currentCount = hmap.get(c);
-                hmap.put(c, currentCount + 1);
-
-            } else {
-                hmap.put(c, 1);
-            }
+            helper[c]++;
         }
         boolean flag = true;
-        for(Character c : hmap.keySet()){
-            int count = hmap.get(c);
-            if(count%2==1 &&flag==true){
-                flag=false;
-            }else if(count%2==1 &&flag==false){
+        for (int i : helper) {
+            if (i % 2 == 1 && flag == true) {
+                flag = false;
+            } else if (i % 2 == 1 && flag == false) {
                 return false;
             }
         }
