8f7ec0ef557f592c9088d998cb4082e5222de960
nikhilay <nikhilayyagari15@gmail.com>
2016-12-29 15:43:05 -0500

    lowes common ancestor in bst solution

---
+++ src/LowestCommonAncestorofABinarySearchTree.java
@@ -0,0 +1,31 @@
+/**
+ * Created by Nikhil on 12/29/16.
+ */
+
+/**
+ * Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+ * According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes v and w
+ * as the lowest node in T that has both v and w as descendants (where we allow a node to be a descendant of itself).”
+ */
+public class LowestCommonAncestorofABinarySearchTree {
+                                                                                                                                                                                                                                                                                                                                                                                     public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if( p.val<root.val && q.val<root.val){
+           return lowestCommonAncestor(root.left,p,q);
+        }else if(p.val>root.val && q.val>root.val){
+            return lowestCommonAncestor(root.right,p,q);
+        }else{
+            return root;
+        }
+
+    }
+}
